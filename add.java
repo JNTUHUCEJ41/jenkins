385fe997c66f8a3e3e4b82e9695f8ac7576759c2
JNTUHUCEJ41 <ucejcse41@gmail.com>
2024-12-11 16:08:01 +0530

    Update add.java

--- add.java
+++ add.java
@@ -7,6 +7,6 @@ public class add{
          int num1 = Integer.parseInt(args[0]);
          int num2 = Integer.parseInt(args[1]);
          int sum = num1 + num2;
-         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+         System.out.println("The sum of" + num1 + "and" + num2 + " is:" + sum);
     }
 }
