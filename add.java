b8a57843b74bde7db9c2669f6446956457f1bd78
JNTUHUCEJ41 <ucejcse41@gmail.com>
2024-12-11 15:58:10 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,26 +1,20 @@
-import java.util.Scanner;  // Import Scanner class for user input
-
 public class add{
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the user
-        Scanner scanner = new Scanner(System.in);
+    if (args.length <2) {
+     System.out.println("please provide two numbers as arguments.");
+     return;
+     }
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
         
-        // Prompt the user to enter the first number
-        System.out.print("Enter the first number: ");
-        double num1 = scanner.nextDouble();
         
-        // Prompt the user to enter the second number
-        System.out.print("Enter the second number: ");
-        double num2 = scanner.nextDouble();
-        
+       
         // Add the two numbers
-        double sum = num1 + num2;
+        int sum = num1 + num2;
         
         // Display the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The sum of " + num1 +" and " num2 + " is: " + sum);
         
-        // Close the scanner
-        scanner.close();
+        
     }
 }
-
